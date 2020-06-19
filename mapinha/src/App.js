7e3ca33cf0bc003a09d9df7df44b57142b7cd5ec
biawar <bianca.guerra@ci.ufpb.br>
2020-06-18 21:47:33 -0300

    second marker and polyline

--- mapinha/src/App.js
+++ mapinha/src/App.js
@@ -40,13 +40,41 @@ class App extends React.Component {
     return null;
   }
 
+  async teste2(res) {
+    try {
+      console.log('Aqui', res)
+      const results = await geocodeByPlaceId(res)
+      console.log('geocodeByPlaceid', results)
+      const latLng = await getLatLng(results[0]);
+      console.log('Aqui', latLng)
+      this.setState({ lat2: latLng.lat, lng2: latLng.lng })
+      //   console.log('Successfully got latitude and longitude', { lat, lng }),
+      // );
+      return latLng;
+    } catch (error) {
+      console.log('Erro:', error);
+    }
+    return null;
+  }
+
+
   render() {
+    const pathCoordinates = [
+      { lat: this.state.lat, lng: this.state.lng },
+      { lat: this.state.lat2, lng: this.state.lng2 }
+    ];
     return (
       <div className="box">
         <div className="containerLateralSide">
           <h1>Digite o local</h1>
           <GooglePlacesAutocomplete
             onSelect={res => this.teste(res.place_id)}
+          // onSelect= {this.state.name}
+          // onClick={this.teste()}
+          />
+
+          <GooglePlacesAutocomplete
+            onSelect={res => this.teste2(res.place_id)}
           // onSelect= {this.state.name}
           // onClick={this.teste()}
           />
@@ -63,8 +91,28 @@ class App extends React.Component {
             <Marker position={{ lat: this.state.lat, lng: this.state.lng }}
               onClick={this.onMarkerClick}
               draggable={true}
-            // position={bagulho => console.log("OLHA AQUI", bagulho)}
-            ></Marker>
+            />
+            <Marker position={{ lat: this.state.lat2, lng: this.state.lng2 }}
+              onClick={this.onMarkerClick}
+              draggable={true}
+            />
+
+            <Polyline
+              path={pathCoordinates}
+              geodesic={true}
+              options={{
+                strokeColor: "#ff2527",
+                strokeOpacity: 0.75,
+                strokeWeight: 2,
+                // icons: [
+                //   {
+                //     icon: lineSymbol,
+                //     offset: "0",
+                //     repeat: "20px"
+                //   }
+                // ]
+              }}
+            />
           </Map>
         </div>
 
